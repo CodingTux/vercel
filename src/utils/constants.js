70ef9887940d33186270f4cddfa3262255ae1c8d
CodingTux <abhishek.vishwakarma9@gmail.com>
2021-06-06 18:58:39 +0530

    updated api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const BASE_API = "http://localhost:5000/api"
+const BASE_API = "https://mm8erc4nnk.execute-api.ap-south-1.amazonaws.com/dev/api"
 export const UPLOAD_ENDPOINT = BASE_API + "/upload"
 export const REGISTER_ENDPOINT = BASE_API + "/register"
 export const FETCH_FILES = BASE_API + "/fetch"
